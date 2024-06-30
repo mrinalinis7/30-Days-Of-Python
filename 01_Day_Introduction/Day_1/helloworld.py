16b8f04e6f99dc865171d0178104db5e28a4f093
tina.d <tina.sd7@gmail.com>
2024-06-30 18:50:22 +1000

    first lesson

---
+++ 01_Day_Introduction/Day_1/helloworld.py
@@ -0,0 +1,22 @@
+print (3+2)
+print (3-2)
+print (3/2)
+print (3*2)
+print (3%2)
+print (3//2)
+print (3**2)
+
+# String prints
+print ("Tina")
+print ("Sydney")
+print (" I am learning python")
+
+# check data type
+print (type(10))
+print (type(9.8))
+print (type(4-4j))
+print (type(['Asabeneh', 'Python', 'Finland']))
+print (type('tina'))
+print (type({'name':'Agastya'}))
+print(type({9.8,'tina', 2.7}))    # Set
+print(type((9.8,'tina', 2.7))) 
