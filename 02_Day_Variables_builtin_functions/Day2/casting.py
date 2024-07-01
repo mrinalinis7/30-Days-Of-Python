4ea2c5a9270a5a004ac67c7b5b9584dbaea604ec
tina.d <tina.sd7@gmail.com>
2024-07-01 22:32:39 +1000

    Done

---
+++ 02_Day_Variables_builtin_functions/Day2/casting.py
@@ -0,0 +1,7 @@
+#Checking Data types and Casting.  Converting one data type to another data type. 
+
+r=input ('Enter Radius')
+area_of_circle = 3.14 * int(r) *int(r)
+print (area_of_circle)
+
+
---
+++ 02_Day_Variables_builtin_functions/Day2/exercise1.py
@@ -0,0 +1,23 @@
+# 'Day 2: 30 Days of python programming'
+first_name ='Mrinalini'
+last_name = 'Dubey'
+age = 45
+year = 1979
+is_married = True
+name, age, country = 'Tina', 45, 'AU'
+
+print (type(first_name))
+print(type(3.14))           # float
+print(type(1 + 1j))         # complex
+print(type(True))           # bool
+print(type([1, 2, 3, 4]))     # list
+print(type({'name':'Asabeneh','age':250, 'is_married':250}))    # dict
+print(type((1,2)))                                              # tuple
+print(type(zip([1,2],[3,4])))                                   # set                                 # set
+
+# str to list
+first_name = 'Mrinalini'
+print(first_name)              
+first_name_to_list = list(first_name)
+print(first_name_to_list)            # ['A', 's', 'a', 'b', 'e', 'n', 'e', 'h']
+
