8883a30190c9ffe5f1b5addaf7c98b24cdb1d78f
tina.d <tina.sd7@gmail.com>
2024-07-02 21:07:41 +1000

    up

---
+++ 03_Day_Operators/3Day.py
@@ -0,0 +1,28 @@
+#Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle 
+# (area = 0.5 x b x h).
+
+
+#b=int (input ('Enter Base'))
+#h = int (input("Enter Height"))
+#area = 0.5 * b *h
+#print(area)
+
+print ('jargon' in 'I hope this course is not full of jargon')
+print ('on' not in 'dragon')
+print ('on' not in 'python')
+a_len = float(len('python'))
+print (str(a_len))
+fl = 7//3
+d = int (2.7)
+
+print (fl==d)
+
+a=int(input('Enter number'))
+if (a%2 == 0):
+  print ('even')
+else:
+  print ('Odd')
+
+Hrs = float (input ('enter hours'))
+Rate = float (input ('enter rate per hr'))
+print ('pay of the person', Hrs*Rate)
